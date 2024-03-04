d6a0fe3ebd8cd0c4c8c17a7aa848bd8e917559e6
Apollo12345678 <weiszapollo@gmail.com>
2024-03-04 21:29:16 +0100

    Train-Ticket: fix docker build image process

--- hack/build-image.sh
+++ hack/build-image.sh
@@ -4,12 +4,13 @@ set -eux
 echo
 echo "Start build images, Repo: $1, Tag: $2"
 echo
+cd "$(dirname "$0")/.." || exit 1
 for dir in ts-*; do
     if [[ -d $dir ]]; then
         if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
             echo "build ${dir}"
-            docker build -t "$1"/"${dir}" "$dir"
-            docker tag "$1"/"${dir}":latest "$1"/"${dir}":"$2"
+            docker build -t "$1"/"${dir}":latest "$dir"
+            #docker tag "$1"/"${dir}":latest "$1"/"${dir}":"$2"
         fi
     fi
 done
